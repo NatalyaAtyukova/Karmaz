1ae33999694475cc2f47db2204a3191650dce314
AtyukovaNatalya <whitepanda205@icloud.com>
2023-08-02 17:49:27 +0300

    Add code in Reg/Log View Controller. - check password - check phone number - check f/l name - have +7 id for number phone and 12-ich size

--- Karmaz/LoginViewController.swift
+++ Karmaz/LoginViewController.swift
@@ -1,29 +1,72 @@
-//
-//  LoginViewController.swift
-//  Karmaz
-//
-//  Created by Наталья Атюкова on 31.07.2023.
-//
+    //
+    //  LoginViewController.swift
+    //  Karmaz
+    //
+    //  Created by Наталья Атюкова on 31.07.2023.
+    //
 
-import UIKit
+    import UIKit
 
-class LoginViewController: UIViewController {
-
+class LoginViewController: UIViewController, UITextFieldDelegate {
+    
+    //@IBOutlet weak var txtFName: UITextField!
+    
+    @IBOutlet weak var loginPhoneNumber: UITextField!
+    
+    @IBOutlet weak var loginPasswordField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Установите делегата loginPhoneNumber в self
+        loginPhoneNumber.delegate = self
+        // Установка значения по умолчанию для поля loginPhoneNumber
+        loginPhoneNumber.text = "+7"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // Get the current text in the text field
+        guard let currentText = textField.text else {
+            return true
+        }
+        
+        // Check if the new text will exceed the character limit
+        let newLength = currentText.count + string.count - range.length
+        return newLength <= 12
     }
-    */
-
+    
+    
+    @IBAction func btnForReg(_ sender: UIButton) {
+        
+        //Переход по кнопке
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let regViewController = storyboard.instantiateViewController(identifier: "RegViewController") as? RegViewController else { return }
+        regViewController.modalPresentationStyle = .fullScreen
+        present(regViewController, animated: true, completion: nil)
+        
+    }
+    
+    
+    @IBAction func btnLoginApp(_ sender: Any) {
+    
+    if (loginPhoneNumber.text?.count == 0){
+        let alert = UIAlertController(title: "Oops!", message: "Please enter your phone id.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    } else if !isValidPhoneNumber(testStr:loginPhoneNumber.text!){
+        let alert = UIAlertController(title: "Oops!", message: "Please enter your correct phone id.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+        //Валидация телефона
+        func isValidPhoneNumber(testStr:String) -> Bool {
+            let phoneRegEx = "^\\+7\\d{10}$"
+            
+            let phoneTest = NSPredicate(format:"SELF MATCHES %@", phoneRegEx)
+            let result = phoneTest.evaluate(with: testStr)
+            return result
+        }
+    }
 }
+    
+
--- Karmaz/RegViewController.swift
+++ Karmaz/RegViewController.swift
@@ -6,25 +6,105 @@
 //
 
 import UIKit
+import PhoneNumberKit
 
-
-class RegViewController: UIViewController {
-
+class RegViewController: UIViewController, UITextFieldDelegate {
+    
+    
+    
+    //@IBOutlet weak var txtFName: UITextField!
+    
+    @IBOutlet weak var txtFName: UITextField!
+    
+    @IBOutlet weak var txtLName: UITextField!
+    
+    @IBOutlet weak var txtPhoneNumber: UITextField!
+    
+    @IBOutlet weak var txtPassword: UITextField!
+    
+    @IBOutlet weak var txtConfirmPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        // Установите делегата txtPhoneNumber в self
+        txtPhoneNumber.delegate = self
+        // Установка значения по умолчанию для поля txtPhoneNumber
+        txtPhoneNumber.text = "+7"
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        // Get the current text in the text field
+        guard let currentText = textField.text else {
+            return true
+        }
+        
+        // Check if the new text will exceed the character limit
+        let newLength = currentText.count + string.count - range.length
+        return newLength <= 12
     }
-    */
 
+    
+    //@IBAction func btnRegistration(sender: AnyObject)
+    
+    @IBAction func btnRegistration(_ sender: Any) {
+        if(txtFName.text?.count == 0){
+            let alert = UIAlertController(title: "Oops!", message: "Please enter your first name.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        } else if (txtLName.text?.count == 0){
+            let alert = UIAlertController(title: "Oops!", message: "Please enter your last name.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            //*
+        } else if (txtPhoneNumber.text?.count == 0){
+            let alert = UIAlertController(title: "Oops!", message: "Please enter your phone id.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        } else if !isValidPhoneNumber(testStr:txtPhoneNumber.text!){
+            let alert = UIAlertController(title: "Oops!", message: "Please enter your correct phone id.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            //*
+        } else if (txtPassword.text?.count == 0){
+            let alert = UIAlertController(title: "Oops!", message: "Please enter your password.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        } else if (txtConfirmPassword.text?.count != txtPassword.text?.count){
+            let alert = UIAlertController(title: "Oops!", message: "Confirmed password not matched please try again.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        } else{
+            let alert = UIAlertController(title: "User Registration!", message: "Your Registration is successfully.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+//    private func textFieldShouldReturn(textField: UITextField) -> Bool
+//    {
+//        if(textField.returnKeyType == UIReturnKeyType.done)
+//        {
+//            textField .resignFirstResponder()
+//        }
+//        else
+//        {
+//            let txtFld : UITextField? = self.view.viewWithTag(textField.tag + 1) as? UITextField;
+//            txtFld?.becomeFirstResponder()
+//        }
+//        return true
+//    }
+    
+    func isValidPhoneNumber(testStr:String) -> Bool {
+        let phoneRegEx = "^\\+7\\d{10}$"
+        
+        let phoneTest = NSPredicate(format:"SELF MATCHES %@", phoneRegEx)
+        let result = phoneTest.evaluate(with: testStr)
+        return result
+    }
+    
 }
+    
