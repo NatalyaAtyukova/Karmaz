208070e440c1cbb887f90eb637a354294c92940d
AtyukovaNatalya <whitepanda205@icloud.com>
2023-08-13 17:43:10 +0300

    add validation for email and password

--- Karmaz/ViewControllers(VC)/RegViewController.swift
+++ Karmaz/ViewControllers(VC)/RegViewController.swift
@@ -62,6 +62,7 @@ class RegViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func btnRegistration(_ sender: Any) {
         
+        //fname and lname
         if txtFName.text?.count == 0 {
             let alert = UIAlertController(title: "Ошибка!", message: "Please enter your first name.", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
@@ -79,13 +80,26 @@ class RegViewController: UIViewController, UITextFieldDelegate {
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             present(alert, animated: true, completion: nil)
             
+            //email
         } else if (txtEmail.text?.count == 0){
             let alert = UIAlertController(title: "Ошибка!", message: "Please enter your Email.", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             present(alert, animated: true, completion: nil)
             
+        } else if !isValidEmail(txtEmail.text!) {
+            let alert = UIAlertController(title: "Ошибка!", message: "Invalid Email. Only alphabetic characters are allowed.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            
+            
+            //password and re-pass
         } else if (txtPassword.text?.count == 0){
             let alert = UIAlertController(title: "Ошибка!", message: "Please enter your password.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            
+        } else if !isValidPassword(txtPassword.text!) {
+            let alert = UIAlertController(title: "Ошибка!", message: "Ваш пароль должен содержать минимум 8 символов, одну заглавную букву, строчную букву и одну цифру", preferredStyle: .alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
             present(alert, animated: true, completion: nil)
             
@@ -142,7 +156,6 @@ class RegViewController: UIViewController, UITextFieldDelegate {
                 }
                 
             }
-
             
         }
         
@@ -156,14 +169,38 @@ class RegViewController: UIViewController, UITextFieldDelegate {
             let result = nameTest.evaluate(with: testStr)
             return result
         }
+    
+    func isValidEmail(_ email: String) -> Bool {
+        // Паттерн для проверки email адреса
+        let emailRegex = "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailRegex)
+        return emailPredicate.evaluate(with: email)
+    }
+
+    func isValidPassword(_ password: String) -> Bool {
+        // Длина пароля должна быть не менее 8 символов
+        if password.count < 8 {
+            return false
+        }
         
-    //pass valid
-    //email valid
-    
-    
-    //FBCreateURLForIdentificateNewUser
-    
-//Auth.auth().createUser(withEmail: txtEmail.text!, password: txtPassword.text!) { authResult, error in
+        // Проверяем, содержит ли пароль хотя бы одну заглавную букву
+        let capitalLetterRegEx  = ".*[A-Z]+.*"
+        let capitalLetterPred = NSPredicate(format:"SELF MATCHES %@", capitalLetterRegEx)
+        let containsCapitalLetter = capitalLetterPred.evaluate(with: password)
+        
+        // Проверяем, содержит ли пароль хотя бы одну строчную букву
+        let smallLetterRegEx  = ".*[a-z]+.*"
+        let smallLetterPred = NSPredicate(format:"SELF MATCHES %@", smallLetterRegEx)
+        let containsSmallLetter = smallLetterPred.evaluate(with: password)
+        
+        // Проверяем, содержит ли пароль хотя бы одну цифру
+        let numberRegEx  = ".*[0-9]+.*"
+        let numberPred = NSPredicate(format:"SELF MATCHES %@", numberRegEx)
+        let containsNumber = numberPred.evaluate(with: password)
+        
+        return containsCapitalLetter && containsSmallLetter && containsNumber
+    }
+
     
         
     }
