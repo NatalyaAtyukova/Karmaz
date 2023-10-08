f023776c0bda8a0a528c7ec10017bab10a5b18c2
AtyukovaNatalya <atukowa.natalya@yandex.ru>
2023-10-08 14:28:59 +0300

    test comm

--- Karmaz/TabBar/OrderViewController.swift
+++ Karmaz/TabBar/OrderViewController.swift
@@ -40,82 +40,10 @@ class OrderViewController: UIViewController, UITableViewDelegate, UITableViewDat
         return cell
     }
     
-    var citiesArray: [String] = []
-
-       // Метод, вызываемый при вводе текста в поле поиска
-       func searchForCity(with keyword: String) {
-           // Выполнение поиска на основе ключевого слова и обновление таблицы с результатами
-           
-           let filteredCities = citiesArray.filter { $0.range(of: keyword, options: .caseInsensitive) != nil }
-           //
-       }
-       
-       func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-           // Вызывайте ваш метод searchForCity при изменении текста в поле поиска
-           searchForCity(with: searchText)
-       }
-       
-       func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-           // Вызывайте метод searchForCity при нажатии на кнопку "Search" на клавиатуре
-           searchForCity(with: searchBar.text ?? "")
-       }
-       
-       func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-           // Выполните нужные действия по отмене поиска
-       }
-       
-       func processCities(cities: [[String: Any]]) {
-           // Очищаем массив городов перед обновлением
-           citiesArray.removeAll()
-           
-           // Добавляем названия городов в массив
-           for city in cities {
-               if let cityName = city["CityName"] as? String {
-                   citiesArray.append(cityName)
-               }
-           }
-           
-           // Выполняем дополнительные действия, если необходимо
-       }
-       
-       
-       
-       
-       
-       func fetchCities() {
-           // Токен для доступа к API
-           let token = "eEGr3r6e2H3Q3ZRHBfnsrYBY3SEYF2Ka"
-           
-           // URL для получения списка городов с API kladr.ru
-           let apiUrl = URL(string: "https://kladr-api.ru/api.php?action=getCities&token=&#40;token)")!
-           
-           let task = URLSession.shared.dataTask(with: apiUrl) { (data, response, error) in
-               if let error = error {
-                   // Обработка ошибки при выполнении запроса к API
-                   print("Ошибка при получении списка городов:", error)
-               }
-               
-               if let data = data {
-                   do {
-                       // Парсинг полученных данных в формате JSON
-                       let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
-                       
-                       // Обработка успешного ответа от API
-                       if let cities = json?["Items"] as? [[String: Any]] {
-                           // Дальнейшая обработка полученного списка городов
-                           self.processCities(cities: cities)
-                       }
-                   } catch {
-                       // Обработка ошибок при парсинге данных
-                       print("Ошибка при разборе данных:", error)
-                   }
-               }
-           }
-           
-           task.resume()
-       }
     
-           
+    
+
+ 
        
    }
 
